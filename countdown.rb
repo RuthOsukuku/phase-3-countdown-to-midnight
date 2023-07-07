cc380aaf0e73c7cca1d479fe8cdc14c3af39b8c5
Ruth <osukukuruth@gmail.com>
2023-07-07 16:21:58 -0400

    Done Assignment

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
 #write your code here
 
-def countdown
-end
+def countdown(number)
+    while number > 0
+      puts "#{number} SECOND(S)!"
+      number -= 1
+    end
+    "HAPPY NEW YEAR!"
+  end
+  
+  def countdown_with_sleep(number)
+    while number > 0
+      puts "#{number} SECOND(S)!"
+      sleep(1)  # Pause for one second
+      number -= 1
+    end
+    "HAPPY NEW YEAR!"
+  end
+  
+  # Example usage:
+  puts countdown(5)
+  puts countdown_with_sleep(5)
+  
